ed289db274b875bb22dbcbe8cc0674647f7b7b72
Eric Dennison <ericd@netdenizen.com>
2018-09-12 11:31:33 -0400

    Updated from Brython Server: 9/12/2018 11:31:32 AM

--- tangentcircles.py
+++ tangentcircles.py
@@ -5,6 +5,9 @@ Sources: W Tucker
 
 """
 
-from math import arccos, pi
+from math import  acos, pi
 
-print(dir(math))
+# angle between circles n and n+1, shrink factor "a"
+th = lambda n, a: acos(((1+a**(n+1))**2+(1+a**n)**2-(a**(n+1)+a**n)**2)/(2*(1+a**(n+1))*(1+q**n)))
+
+print(th(0,.9))
