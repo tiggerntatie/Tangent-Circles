924bdb11e638ed1c8e498ca929429213d0d700bf
Eric Dennison <ericd@netdenizen.com>
2018-09-12 11:46:29 -0400

    Updated from Brython Server: 9/12/2018 11:46:29 AM

--- tangentcircles.py
+++ tangentcircles.py
@@ -10,4 +10,4 @@ from math import  acos, pi
 # angle between circles n and n+1, shrink factor "a"
 th = lambda n, a: acos(((1+a**(n+1))**2+(1+a**n)**2-(a**(n+1)+a**n)**2)/(2*(1+a**(n+1))*(1+a**n)))
 
-print(th(0,.9))
+print(th(0,.5))
