cc329a0a64c9c64ef52b3cd0974d19605831ce47
Eric Dennison <ericd@netdenizen.com>
2018-09-12 12:46:09 -0400

    Updated from Brython Server: 9/12/2018 12:46:08 PM

--- tangentcircles.py
+++ tangentcircles.py
@@ -5,9 +5,37 @@ Sources: W Tucker
 
 """
 
-from math import  acos, pi
+from ggmath import MathApp, Circle
+from math import  acos, pi, cos, sin
+
 
 # angle between circles n and n+1, shrink factor "a"
 th = lambda n, a: acos(((1+a**(n+1))**2+(1+a**n)**2-(a**(n+1)+a**n)**2)/(2*(1+a**(n+1))*(1+a**n)))
 
-print(th(0,.5))
+# distance to center of nth circle, shrink factor "a", base circle radius r
+d = lambda n, a, r: r*(1 + a**(n+1))
+
+# total angle for center of nth circle (recursive)
+thtot = lambda n, a: 0 if n == 0 else th(n,a) + th(n-1,a)
+
+# position of center of nth circle, shrink factor a, base circle radius r
+pos = def(n, a, r):
+    r = d(n, a, r)  # get distance to center
+    angle = thtot(n,a)  # angle to center
+    return (r*cos(angle), r*sin(angle))
+
+circleqty = 5
+a = 0.8
+r = 2
+angle = 0
+
+# draw the base circle
+Circle((0,0), r)
+
+# draw the children
+for n in range(circleqty):
+    Circle(pos(n, a, r), r*a**(n+1))
+
+
+app = MathApp()
+app.run()
